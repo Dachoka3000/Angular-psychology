dc30d1739463fc0c0151bc396fde2c6d8a55cb97
Kamotho Sheila <kamothosheila@gmail.com>
2020-11-05 07:42:18 +0300

    add styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import {EditProfileComponent } from './edit-profile/edit-profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent},
+  { path: 'edit-profile', component: EditProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
