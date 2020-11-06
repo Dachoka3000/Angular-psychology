0a7636d1c0ddf822b7f6dbf0c219b009dba05bca
Kamotho Sheila <kamothosheila@gmail.com>
2020-11-06 11:05:44 +0300

    add changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { HighlightDirective } from './highlight.directive';
+import { DayCountPipe } from './day-count.pipe';
+import { FormsModule } from '@angular/forms';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ProfileComponent,
+    QuoteComponent,
+    QuoteFormComponent,
+    HighlightDirective,
+    DayCountPipe,
+    QuoteDetailComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
